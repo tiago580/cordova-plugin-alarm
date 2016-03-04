8d4a942d22c0a960d1b6a72d7cbda89f69d6f9dc
tiago.rocha <tiago580@gmail.com>
2016-03-04 17:40:49 -0300

    alteracoes

--- src/android/Alert.java
+++ src/android/Alert.java
@@ -16,21 +16,23 @@ public class Alert extends CordovaPlugin{
     }
 
     public boolean execute(String action, JSONArray args, CallbackContext context) throws JSONException{
-        if(action.equals("alert")){
-
+        if (action.equals("alert")) {
+            alert(args.getString(0), args.getString(1), args.getString(2), callbackContext);
+            return true;
         }
+        return false;
     }
 
     private synchronized void alert(final String title, final String message, final String buttonLabel, final CallbackContext callbackContext){
          new AlertDialog.Builder(cordova.getActivity())
-        .setTitle(title)
-        .setMessage(message)
-        .setCancelable(false)
-        .setNeutralButton(buttonLabel, new AlertDialog.OnClickListener(){
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-                callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, 0));
+                 .setTitle(title)
+                 .setMessage(message)
+                 .setCancelable(false)
+                 .setNeutralButton(buttonLabel, new AlertDialog.OnClickListener() {
+                     @Override
+                     public void onClick(DialogInterface dialog, int which) {
+                         dialog.dismiss();
+                         callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, 0));
             }
         })
         .create()
